e6a7f350ad80d631c0fdf55b0060f34da47d1b89
Kevin Yang <kuan_yang@mfe.berkeley.edu>
2013-12-07 16:37:16 -0800

    Add static folder containing a pic and defined the settings

--- tango/settings.py
+++ tango/settings.py
@@ -5,6 +5,7 @@ PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
 PROJECT_PATH = os.path.abspath(PROJECT_PATH)
 
 TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
+STATIC_PATH = os.path.join(PROJECT_PATH, 'static')
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -78,6 +79,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    STATIC_PATH,
 )
 
 # List of finder classes that know how to find static files in
