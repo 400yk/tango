d22743e53a269677d2666dbdf3867a9e3d6c8e2d
Kevin Yang <kuan_yang@mfe.berkeley.edu>
2013-12-07 21:53:25 -0800

    Add admin page

--- tango/urls.py
+++ tango/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -17,6 +17,7 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
 
     url(r'^rango/', include('rango.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
