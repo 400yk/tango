d5d74645a31b253586527405484e687c9b5e9434
Kevin Yang <kuan_yang@mfe.berkeley.edu>
2013-12-07 16:12:36 -0800

    Add a simple template and index.html

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("Rango says hello world! <a href='/rango/about/'>About</a>")
+    # Request the context of the request.
+    # The context contains information such as the client's machine details, for example.            
+    context = RequestContext(request)
+
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    # Used shortcut here: render_to_response
+    return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
     return HttpResponse("Range says: here is the about page. <a href='/rango/'>Main page</a>")
