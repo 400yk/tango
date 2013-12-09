7b4f40645eae1f8df585dea3e11d262558ca2906
Kevin Yang <kuan_yang@mfe.berkeley.edu>
2013-12-08 18:01:51 -0800

    Finish Chapter 5, customize admin page

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
-admin.site.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+    search_fields = ['title']
+
+
+class PageInline(admin.TabularInline):
+    model = Page
+    extra = 1
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'views', 'likes')
+    search_field = ['name']
+    list_filter = ['views']
+    inlines = [PageInline]
+
+
+admin.site.register(Category, CategoryAdmin)
+#admin.site.register(Page, PageAdmin)
+
 
