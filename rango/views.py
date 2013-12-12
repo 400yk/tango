3fa2edfa7678ce5ed8d00f0b5c039de960caf0b8
Kevin Yang <kuan_yang@mfe.berkeley.edu>
2013-12-11 21:29:47 -0800

    Add category page, use name for the url, add link from index page to the category page

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
+from rango.models import Category, Page
+
 def index(request):
     # Request the context of the request.
     # The context contains information such as the client's machine details, for example.            
@@ -9,7 +11,12 @@ def index(request):
 
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    category_list = Category.objects.order_by("-likes")[:5]
+    context_dict = {'categories': category_list}
+
+    for category in category_list:
+        category.url = category.name.replace(' ', '_')
 
     # Used shortcut here: render_to_response
     return render_to_response('rango/index.html', context_dict, context)
@@ -18,5 +25,25 @@ def about(request):
     context = RequestContext(request)
     about_message = {'self_intro': "Hi there it's fun programming in Django!"}
     return render_to_response('rango/about.html', about_message, context)
+
+def category(request, category_name_url):
+    context = RequestContext(request)
+    # URL doesn't handle space well, we encode them into underscores
+    category_name = category_name_url.replace('_', ' ')
+    context_dict = {'category_name' : category_name}
+
+    try:
+        category = Category.objects.get(name = category_name)
+        pages = Page.objects.filter(category = category)
+
+        context_dict['pages'] = pages
+
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        pass
+
+    return render_to_response('rango/category.html', context_dict, context)
+
 
 
