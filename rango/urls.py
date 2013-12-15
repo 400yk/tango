dc9bf2ca946889e0c2cd51c11dc41fb506301b91
Kevin Yang <kuan_yang@mfe.berkeley.edu>
2013-12-15 14:15:50 -0800

    Add user login

--- rango/urls.py
+++ rango/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
         url(r'^add_category/$', views.add_category, name = 'add_category'),
         url(r'^add_page/(?P<category_name_url>\w+)/$', views.add_page, name = 'add_page'),
         url(r'^register/$', views.register, name = 'register'),
+        url(r'login/$', views.user_login, name = 'login'),
         url(r'^category/(?P<category_name_url>\w+)/$', views.category, name='category'),) # New!
--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect, HttpResponse
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
@@ -146,3 +147,28 @@ def register(request):
         'profile_form': profile_form,
         'registered': registered
         }, context)
+
+def user_login(request):
+    context = RequestContext(request)
+
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # User django's machinary to see if the username and password match record, 
+        # if so it will return a User object
+        user = authenticate(username = username, password = password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/rango/')
+            else:
+                return HttpResponse('Your Rango account is disabled.')
+
+        else:
+            print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render_to_response('rango/login.html', {}, context)
+
